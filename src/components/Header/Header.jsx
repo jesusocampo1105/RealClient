104398f7339e9685c87db3c52ab6eb516342cd0d
Nisanech <57728976+Nisanech@users.noreply.github.com>
2022-12-19 23:45:58 -0500

    Se realiza el header de la aplicación en versión móvil, tablet y escritorio

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { Container } from "reactstrap";
+import Logo from "../../assets/img/Logo.svg";
+import { NavLink, Link } from "react-router-dom";
+import "../../styles/Header.css";
+
+// Navigation Links
+const nav__links = [
+  {
+    display: "Inicio",
+    path: "/home",
+  },
+  {
+    display: "Productos",
+    path: "/products",
+  },
+  {
+    display: "Carrito",
+    path: "/cart",
+  },
+];
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const menuRef = useRef(null)
 
-export default Header
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
+  return (
+    <header className="header">
+      <Container>
+        <div className="nav__wrapper d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <img src={Logo} alt="Logo" />
+          </div>
+
+          {/* Menu */}
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+            <div className="menu d-flex align-items-center gap-5">
+              {nav__links.map((item, index) => (
+                <NavLink
+                  to={item.path}
+                  key={index}
+                  className={(navClass) =>
+                    navClass.isActive ? "active__menu" : ""
+                  }
+                >
+                  {item.display}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+
+          {/* Nav right icons */}
+          <div className="nav__right d-flex align-items-center gap-5">
+            <span className="cart__icon">
+              <i className="ri-shopping-cart-2-fill"></i>
+
+              <span className="cart__badge">2</span>
+            </span>
+
+            <span className="mobile__menu" onClick={toggleMenu}>
+              <i className="ri-menu-line"></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
