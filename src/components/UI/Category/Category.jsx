c9c95531bd7233a2589465841aeb3ad036e133fe
Nisanech <57728976+Nisanech@users.noreply.github.com>
2022-12-21 18:21:07 -0500

    Se realiza sección de categorías

--- src/components/UI/Category/Category.jsx
+++ src/components/UI/Category/Category.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 
+import {Container, Row, Col} from 'reactstrap'
+import '../../../styles/Category.css'
+
+import CategBDSM from '../../../assets/img/Categ-BDSM.png'
+import CategToys from '../../../assets/img/Categ-Juguetes.png'
+import CategLingerie from '../../../assets/img/Categ-Lingerie.png'
+import CategCouple from '../../../assets/img/Categ-Parejas.png'
+
+// Card categories
+const categoryData = [
+  {
+    display: 'Juguetes',
+    image: CategToys
+  },
+
+  {
+    display: 'BDSM',
+    image: CategBDSM
+  },
+
+  {
+    display: 'Lingerie',
+    image: CategLingerie
+  },
+
+  {
+    display: 'Parejas',
+    image: CategCouple
+  }
+]
+
 const Category = () => {
   return (
-    <div>Category</div>
+    <Container>
+      <Row>
+        {/* Generate card category */}
+        {categoryData.map((item, index) => (
+          <Col lg='3' md='4' sm='6' xs='6' key={index} className='mb-5 mt-5'>
+            <div className="category__item d-flex align-items-center gap-3">
+              <div className="category__img">
+                <img src={item.image} alt="Imagen Categoría"/>
+              </div>
+
+              <h6>{item.display}</h6>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Helmet from '../components/Helmet/Helmet'
 
-import {Container, Row, Col} from 'reactstrap'
+// import {Container, Row, Col} from 'reactstrap'
 import '../styles/Home.css'
+
+import Category from '../components/UI/Category/Category'
 
 const Home = () => {
   return (
     <Helmet title='Inicio'>
+      {/* Hero */}
       <section>
         <div className="hero__img"></div>
+      </section>
+
+      {/* Categories */}
+      <section className='pt-0'>
+        <Category/>
       </section>
     </Helmet>
   )
